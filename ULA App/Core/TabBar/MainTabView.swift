3a87bb5e0e247ec027639cca280715ed69e1f7b7
Jarek <jaroslaw-kaminski@outlook.com>
2024-04-30 21:46:25 +0100

    TabBar setup

---
+++ ULA App/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  ULA App
+//
+//  Created by Jarek  on 30/04/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
---
+++ ULA App/Extensions/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  ULA App
+//
+//  Created by Jarek  on 30/04/2024.
+//
+
+import Foundation
