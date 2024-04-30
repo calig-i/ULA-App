2ccd914a7b9fbf6f0348689fc0b9d952649c5f57
Jarek <jaroslaw-kaminski@outlook.com>
2024-04-30 20:53:36 +0100

    Initial Commit

---
+++ ULA App/ULA_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ULA_AppApp.swift
+//  ULA App
+//
+//  Created by Jarek  on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ULA_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
