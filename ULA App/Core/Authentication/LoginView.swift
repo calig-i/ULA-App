bdf2db51d6037b1a1a2e54b1ec0cf3dfc208c61b
Jarek <jaroslaw-kaminski@outlook.com>
2024-05-04 14:04:09 +0100

    Login Screen setup

--- ULA App/Core/Authentication/LoginView.swift
+++ ULA App/Core/Authentication/LoginView.swift
@@ -8,8 +8,135 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            
+            // logo image
+            VStack {
+                Spacer()
+                // logo image
+                Image("ULALoginScreen")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 120, height: 220)
+                    .padding()
+                
+                // text fields
+                VStack {
+                    TextField("Enter your email", text: $email )
+                        .autocapitalization(.none)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                // log in button
+                Button {
+                    print("Login")
+                } label: {
+                    Text("Log In")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 360, height: 44)
+                        .foregroundColor(.white)
+                        .background(Color.tealGreen)
+                        .cornerRadius(10)
+                    
+                }
+                
+                // forgot password button
+                .padding(.vertical)
+                
+                Button {
+                    print("Forgot passsword")
+                } label: {
+                    Text("Forgot password?")
+                        .foregroundColor(Color.tealGreen)
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                
+                // divider "or"
+                HStack {
+                    Rectangle()
+                    .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                    
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                    Rectangle()
+                    .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                }
+                .foregroundColor(.gray)
+                .padding()
+                
+                
+                /// login by facebook and google buttons
+                HStack {
+                    Spacer()
+                    HStack {
+                        Image("facebookLogo")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                        Text("Log In")
+                        
+                    }
+                    Spacer()
+                    
+                    HStack {
+                        Image("googleLogo")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                        Text("Log In")
+                        
+                    }
+                    Spacer()
+                }
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.tealGreen)
+                Spacer()
+                
+            }
+            .padding(.top, 8)
+            
+            Spacer()
+            
+            Divider()
+            
+            // sign up button with navigation link
+            NavigationLink {
+                Text("Sign up")
+            } label: {
+                HStack(spacing: 3) {
+                    Text("Don't have an account?")
+                    
+                    Text("Sign up")
+                        .fontWeight(.semibold)
+                }
+                .font(.footnote)
+                .foregroundColor(Color.tealGreen)
+            }
+            .padding(.vertical, 16)
+
+        }
+        
     }
 }
 
