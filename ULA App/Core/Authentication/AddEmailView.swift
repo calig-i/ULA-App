7d8b02ed67489478497d271ad73528369ed15fe8
Jarek <jaroslaw-kaminski@outlook.com>
2024-05-06 12:11:42 +0100

    Logo without background. Update

---
+++ ULA App/Core/Authentication/AddEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  AddEmailView.swift
+//  ULA App
+//
+//  Created by Jarek  on 06/05/2024.
+//
+
+import SwiftUI
+
+struct AddEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddEmailView()
+}
